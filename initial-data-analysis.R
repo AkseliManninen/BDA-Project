90bcdd96066b9c4393af57c251cc75cfc1f29462
EulersNumber <61505830+EulersNumber@users.noreply.github.com>
2022-11-16 17:06:58 +0200

    Updated file paths

--- initial-data-analysis.R
+++ initial-data-analysis.R
@@ -17,35 +17,35 @@ categorical.vars <- c("LOCALE", "CCBASIC", "CONTROL")
 SAT.vars <- c("SATVRMID", "SATMTMID", "SATWRMID") # helper variable later on
 
 # create map for variable descriptions
-variable.descriptions <- data.description %>% 
-  select(VARIABLE.NAME, NAME.OF.DATA.ELEMENT, NOTES) %>% 
+variable.descriptions <- data.description %>%
+  select(VARIABLE.NAME, NAME.OF.DATA.ELEMENT, NOTES) %>%
   filter(VARIABLE.NAME %in% c(id.vars, categorical.vars, numerical.vars))
 
 # extract categorical variables
-data.filtered.categorical <- data %>% 
-  select(all_of(id.vars), all_of(categorical.vars)) %>% 
+data.filtered.categorical <- data %>%
+  select(all_of(id.vars), all_of(categorical.vars)) %>%
   mutate(across(.cols = all_of(categorical.vars), .fns = as.factor))
 
 # drop rows with NA for categorical vars
-data.filtered.categorical.dropna <- data.filtered.categorical %>% 
+data.filtered.categorical.dropna <- data.filtered.categorical %>%
   drop_na()
 
 # extract numerical variables
-data.filtered.numerical <- data %>% 
+data.filtered.numerical <- data %>%
   select(all_of(id.vars), all_of(numerical.vars)) %>%
   mutate(across(.cols = c(id.vars[1], numerical.vars), .fns = as.numeric))
 
 # drop rows with NA for numerical vars
-data.filtered.numerical.dropna <- data.filtered.numerical %>% 
+data.filtered.numerical.dropna <- data.filtered.numerical %>%
   drop_na()
 
 # join categorical and numerical variables by id
 data.filtered.numerical.dropna <- data.filtered.numerical.dropna %>%
-  mutate(SAT_ALL = data.filtered.numerical.dropna %>% 
-           select(SATVRMID, SATMTMID, SATWRMID) %>% 
+  mutate(SAT_ALL = data.filtered.numerical.dropna %>%
+           select(SATVRMID, SATMTMID, SATWRMID) %>%
            rowMeans())
 
-data.filtered.all <- data.filtered.numerical.dropna %>% 
+data.filtered.all <- data.filtered.numerical.dropna %>%
   inner_join(data.filtered.categorical.dropna, by = id.vars)
 
 # PRELIMINARY ANALYSIS ----
@@ -72,12 +72,12 @@ summary(model)
 
 # step wise regression implied "best" model in terms of AIC
 step(model)
-stepwise.model <- lm(MD_EARN_WNE_P10 ~ FEMALE + C150_4 + COSTT4_A + POVERTY_RATE + 
+stepwise.model <- lm(MD_EARN_WNE_P10 ~ FEMALE + C150_4 + COSTT4_A + POVERTY_RATE +
   UNEMP_RATE + MARRIED + SAT_ALL, data = numerical.vars.data)
 summary(stepwise.model)
 
 # numerical + categorical vars model
-full.model.data <- data.filtered.all %>% 
+full.model.data <- data.filtered.all %>%
   select(!c(id.vars, SAT.vars))
 model.full <- lm(MD_EARN_WNE_P10 ~ ., full.model.data)
 summary(model.full)
