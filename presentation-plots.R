73933b4095719a1dc2d5cdde7385a4077f49a557
EulersNumber <61505830+EulersNumber@users.noreply.github.com>
2022-12-16 12:23:28 +0200

    Cleaned up repo

--- presentation-plots.R
+++
@@ -1,9 +0,0 @@
-hist(data.joined.stan$MD_EARN_WNE_P10, breaks = 30, main = NA, xlab = NA)
-hist(data.joined.stan$SAT_ALL, breaks = 30, main = NA, xlab = NA)
-hist(data.joined.stan$AGE_ENTRY, breaks = 30, main = NA, xlab = NA)
-hist(data.joined.stan$POVERTY_RATE, breaks = 30, main = NA, xlab = NA)
-
-boxplot(data.joined.stan$MD_EARN_WNE_P10)
-boxplot(data.joined.stan$SAT_ALL)
-boxplot(data.joined.stan$AGE_ENTRY)
-boxplot(data.joined.stan$POVERTY_RATE)
